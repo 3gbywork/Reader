1db7d3735342c7b2431605db7a9a15eac6f49acb
3gbywork <614258809@qq.com>
2019-04-01 16:59:21 +0800

    添加项目结构

---
+++ src/ReadMe.md
@@ -0,0 +1,9 @@
+## 项目结构
+
+* Reader.Repository			本地数据仓库
+
+* Reader.Source				书源抽象层
+
+* Reader.Source.Stoneread	石头阅读书源实现
+
+* Reader.UI					用户界面层，使用Winform+Mono便于支持Linux和MacOS
---
+++ src/Reader.Source.Stoneread/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reader.Source.Stoneread
+{
+    public class Class1
+    {
+    }
+}
