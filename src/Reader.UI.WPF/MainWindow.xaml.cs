f2a6b98fb37e89a658708d14a39752960e92ceb7
3gbywork <614258809@qq.com>
2019-05-11 18:08:08 +0800

    添加WPF项目

---
+++ src/Reader.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Reader.Model.Types;
+using Reader.Source.Zhuishushenqi.Services;
+using System.Windows;
+
+namespace Reader.UI.WPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var service = new CategoryService();
+            Categories categories = await service.GetCategoriesAsync();
+
+            if (!categories.OK)
+                return;
+        }
+    }
+}
